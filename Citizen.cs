01f6e54a062af270c2022eece67ce83a85198948
Csharp062020 <67912065+Csharp062020@users.noreply.github.com>
2020-07-13 03:14:47 +0300

    HW12072020

---
+++ Citizen.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace HomeWorks
+{
+    class Citizen
+    {
+        public static int _lastIDWeHave=0;
+        public string _name;
+        public Citizen[] _children;
+        public readonly int _fatherID;
+        public readonly int _id= IDCreatorAndCitizenCounter();
+        public static int _numberOfCurrentCitizens=0;
+        public const int _maximumNumberOfCitizens=3;
+
+        public static int IDCreatorAndCitizenCounter()
+        {
+            _lastIDWeHave += 8;
+            _numberOfCurrentCitizens++;
+            return _lastIDWeHave;
+
+        }
+        public Citizen(string _name, int _fatherID)
+        {
+            this._name = _name;
+            this._fatherID = _fatherID;
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"number of citizen : {_numberOfCurrentCitizens}");
+        }
+
+        public static bool ReachedHakfOfMaximumSize()
+        {
+            return (_numberOfCurrentCitizens >= _maximumNumberOfCitizens);
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine($"ID : {_id}");
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine($"Gap Between My ID ({_id}) And Father ID ({_fatherID}) is: {_id-_fatherID}");
+        }
+        
+        public void SetChildren(Citizen[] citizensArray)
+        {
+            _children = citizensArray;
+        }
+
+        public override string ToString()
+        {
+            string children="";
+            if (_children != null)
+            {
+                for (int i = 0; i < _children.Length; i++)
+                {
+                    children = $"{children} {_children[i]._id}";
+                }
+            }
+            else
+            {
+                children = "No Children";
+            }
+
+            return $"{base.ToString()} Name: {_name} , Children IDs: {children} , Father ID {_fatherID} , ID {_id} , Number of current citizens {_numberOfCurrentCitizens} , Max Citizens : {_maximumNumberOfCitizens}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Citizen a = new Citizen("father", 0);
+            Citizen b = new Citizen("levi1", a._id);
+            Console.WriteLine($"status reach maximum ? : {Citizen.ReachedHakfOfMaximumSize()}  max {Citizen._maximumNumberOfCitizens} currect {Citizen._numberOfCurrentCitizens}");
+            Citizen c = new Citizen("levi2", a._id);
+            Console.WriteLine($"status reach maximum ? : {Citizen.ReachedHakfOfMaximumSize()}  max {Citizen._maximumNumberOfCitizens} currect {Citizen._numberOfCurrentCitizens}");
+            Citizen d = new Citizen("levi3", a._id);
+            Citizen e = new Citizen("levi Worng", 1);
+            Console.WriteLine($"status reach maximum ? : {Citizen.ReachedHakfOfMaximumSize()}  max {Citizen._maximumNumberOfCitizens} currect {Citizen._numberOfCurrentCitizens}");
+
+            Console.WriteLine();
+            Citizen[] citizensArray = { b, c, d };
+            Console.WriteLine($"{a._name}  has children: {HasChilldren(a)}");
+
+            Console.WriteLine();
+            Console.WriteLine(a.ToString());
+
+            Console.WriteLine();
+            a.SetChildren(citizensArray);
+            Console.WriteLine($"set children to {a._name}");
+            Console.WriteLine(a.ToString());
+            Console.WriteLine($"{a._name}  has children: {HasChilldren(a)}");
+
+            Console.WriteLine();
+            Console.WriteLine($"now check validity of child to citizen {a._name}  is valid: {CheckValidity(a)}");
+
+            Console.WriteLine();
+            Console.WriteLine("set worng child to father id ");
+            Citizen[] citizensArray1 = { b, c, d,e };
+            a.SetChildren(citizensArray1);
+
+            Console.WriteLine();
+            Console.WriteLine($"now check validity of child to citizen {a._name}  is valid: {CheckValidity(a)}");
+
+            Console.WriteLine();
+            Citizen.PrintNumberOfCitizens();
+            Citizen.ReachedHakfOfMaximumSize();
+            a.PrintID();
+            c.PrintGapBetweenMyIDAndFather();
+            c.ToString();
+
+
+        }
+        static bool HasChilldren(Citizen citizen)
+        {
+            return citizen._children != null;
+        }
+
+        static bool CheckValidity(Citizen citizen)
+        {
+            if (HasChilldren(citizen))
+            {
+                for (int i = 0; i < citizen._children.Length; i++)
+                {
+                    if(citizen._children[i]._fatherID!=citizen._id)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true ;
+        }
+    }
+}
